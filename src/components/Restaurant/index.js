3fcc3f779e662920912bb5364ec71214435299a2
SergeyKhval <sergeykhval@gmail.com>
2016-07-21 18:26:54 +0300

    feat: add ability to get place photos

--- src/components/Restaurant/index.js
+++ src/components/Restaurant/index.js
@@ -3,11 +3,13 @@ import React, {Component, PropTypes} from 'react';
 class Restaurant extends Component {
   render() {
     const {restaurant} = this.props;
+    let imgUrl = restaurant.photos ? restaurant.photos[0].getUrl({'maxWidth': 256, 'maxHeight': 256}) : '';
 
     return (
       <div>
         <h3><a href={`/restaurants/${restaurant.place_id}`}>{restaurant.name}</a></h3>
         <p className='lead'>{restaurant.rating ? restaurant.rating : 'Not rated'}</p>
+        <img src={imgUrl} alt={restaurant.name}/>
       </div>
     )
 
