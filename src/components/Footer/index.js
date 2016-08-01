c2bfbc7e09f96ba2490dd6397031b82ac59205d7
Sergey Khval <sergeykhval@gmail.com>
2016-08-01 23:37:19 +0300

    feat: add  powered by google  (fix #1)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,32 @@
 import React, {Component} from 'react';
 
 import './style.scss';
+import img from '../../assets/powered_by_google_on_white.png';
 
 class Footer extends Component {
   render() {
     return (
       <footer className='footer-main row text-center'>
-        <div className='col-md-4'>
+        <div className='col-md-3'>
           <ul className='nav nav-pills nav-stacked'>
             <li><a href='/'>© 2016 Restik</a></li>
           </ul>
         </div>
-        <div className='col-md-4'>
+        <div className='col-md-3'>
           <ul className='nav nav-pills nav-stacked'>
             <li><a href='https://twitter.com/HiIAmGluk'>Follow us</a></li>
           </ul>
         </div>
-        <div className='col-md-4'>
+        <div className='col-md-3'>
           <ul className='nav nav-pills nav-stacked'>
             <li><a href='mailto:sergeykhval@gmail.com'>Contact us</a></li>
           </ul>
         </div>
+        <col-md-3>
+          <ul className='nav nav-pills nav-stacked'>
+            <li><a href='https://google.com'><img src={img} alt='Google'/></a></li>
+          </ul>
+        </col-md-3>
       </footer>
     )
   }
